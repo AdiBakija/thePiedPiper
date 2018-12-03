ad7b22416ba887617a2d4c1e978ddfb08fd81871
Adi <adibakija35@hotmail.com>
2018-12-03 19:08:30 +0000

    added JSON validation and corrected

--- server.js
+++ server.js
@@ -1,29 +1,48 @@
+'use strict';
+
 const express           = require('express');
 const app               = express();
 const http              = require('http').Server(app);
 const io                = require('socket.io')(http);
-const bodyParser        = require("body-parser");
 const fs                = require('fs');
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html')
-})
+  res.sendFile('index.html');
+});
 
-app.post('/', (req, res) => {
-  console.log(req.body)
-})
+const IsJson = (data) => {
+    try {
+        JSON.parse(data);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
 
 io.on('connection', (socket) => {
   console.log('Client connected.');
   socket.join('update');
 
-  // When new data is incoming broadcast a message to all clients
-  // listening on the update channel.
-  socket.on('UPDATE_DATA', () => {
-    io.to('update').emit('incoming_data', JSON.stringify(data));
+  // When new data is incoming, broadcast a message to all clients
+  // listening on the update channel with the data and write to .txt file.
+  socket.on('UPDATE_DATA', (data) => {
+    //Validate if JSON is valid or not
+    let parsedData = JSON.parse(data);
+    let validateJson = IsJson(parsedData);
+    if (validateJson) {
+      io.to('update').emit('INCOMING_DATA', JSON.stringify(parsedData));
+      //Write JSON data to .txt file
+
+    } else {
+      io.to('update').emit('JSON_ERROR', data);
+    }
+
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected.');
   });
 
 });
