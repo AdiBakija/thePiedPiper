66aacbf71e30a7218c1ed3f9e7cdbb54a2e35ff1
Adi <adibakija35@hotmail.com>
2018-12-04 04:16:45 +0000

    Added data entry and query functionality

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
 
+/*
+  ============
+  DATA HELPERS
+  ============
+*/
+
+//Helper function to validate JSON
 const IsJson = (data) => {
     try {
         JSON.parse(data);
@@ -19,26 +26,87 @@ const IsJson = (data) => {
         return false;
     }
     return true;
-}
+};
+
+//Helper function to serialize JSON.
+const serialize = (data) => {
+  return JSON.stringify(data, null, 2);
+};
+
+/*
+  =========
+  SOCKET.IO
+  =========
+*/
 
 io.on('connection', (socket) => {
   console.log('Client connected.');
   socket.join('update');
 
-  // When new data is incoming, broadcast a message to all clients
-  // listening on the update channel with the data and write to .txt file.
+  // When new JSON data is incoming, broadcast a message to all clients
+  // listening on the update channel with the data and write to JSON file.
   socket.on('UPDATE_DATA', (data) => {
+
     //Validate if JSON is valid or not
-    let parsedData = JSON.parse(data);
-    let validateJson = IsJson(parsedData);
+    //let parsedData = JSON.parse(data);
+    let validateJson = IsJson(data);
     if (validateJson) {
-      io.to('update').emit('INCOMING_DATA', JSON.stringify(parsedData));
-      //Write JSON data to .txt file
+      io.to('update').emit('INCOMING_DATA', data);
+
+      //Buffer of existing contents of JSON file.
+      fs.readFile("./data.json", (err, content) => {
+        //If contents exist in the file, treat it as an array of objects.
+        //Otherwise, create an empty array and add the data.
+        if (content.length !== 0) {
+          let buffer = JSON.parse(content);
+          buffer.push(data);
+          //Write JSON data to file buffer.
+          fs.writeFile("./data.json", serialize(buffer), 'utf-8', (err) => {
+              if (err) {
+                  console.error(err);
+                  return;
+              };
+              console.log("File has been updated.");
+          });
+        } else {
+          let json = [];
+          json.push(data);
+          fs.writeFileSync("./data.json", serialize(json), 'utf-8', (err) => {
+              if (err) {
+                  console.error(err);
+                  return;
+              };
+              console.log("Initial entry has been created.");
+          });
+        }
+      });
 
     } else {
+      //Emit the data back if it's not valid JSON for client side handler
+      //To log appropriate error message.
       io.to('update').emit('JSON_ERROR', data);
     }
 
+  });
+
+  socket.on('QUERY_DATA', (key) => {
+    fs.readFile("./data.json", (err, content) => {
+      //If key exist in the file, return it's value as the result.
+      //Emit the returned result to client side code.
+      if (content.length !== 0) {
+        let buffer = JSON.parse(content);
+        let result;
+        buffer.forEach((obj) => {
+          let parsedObj = JSON.parse(obj);
+          if(parsedObj[key] != undefined) {
+            result = parsedObj[key];
+          }
+        });
+      io.to('update').emit('QUERY_RESULT', result);
+      } else {
+        console.log("There is no data available.");
+      }
+    });
   });
 
   socket.on('disconnect', () => {
@@ -47,4 +115,4 @@ io.on('connection', (socket) => {
 
 });
 
-http.listen(8080, () => console.log('Listening on port 8080.'));
+exports.server = http.listen(8080);
--- test/test.js
+++ test/test.js
@@ -1,1 +1,46 @@
-console.log("this is your future test file");
+'use strict';
+
+const expect = require('chai').expect;
+const server = require('../server.js');
+const io = require('socket.io-client');
+const ioOptions = {
+      transports: ['websocket']
+    , forceNew: true
+    , reconnection: false
+  };
+const testMsg = 'HelloWorld';
+var sender;
+var receiver;
+
+
+
+describe('Socket Events', function(){
+  beforeEach(function(done){
+
+    // start the io server
+    server.start()
+    // connect two io clients
+    sender = io('http://localhost:8080/', ioOptions)
+    receiver = io('http://localhost:8080/', ioOptions)
+
+    // finish beforeEach setup
+    done()
+  })
+  afterEach(function(done){
+
+    // disconnect io clients after each test
+    sender.disconnect()
+    receiver.disconnect()
+    done()
+  })
+
+  describe('Update Events', function(){
+    it('Clients should receive a message when the `message` event is emited.', function(done){
+      sender.emit('message', testMsg)
+      receiver.on('message', function(msg){
+        expect(msg).to.equal(testMsg)
+        done()
+      })
+    })
+  })
+})
